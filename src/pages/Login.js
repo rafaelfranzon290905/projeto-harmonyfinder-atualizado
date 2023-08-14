f9362d911d13f5e92a8b2869757c21ffc3723f39
rafaelfranzon290905 <84185550+rafaelfranzon290905@users.noreply.github.com>
2023-08-14 16:02:54 -0300

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,23 +1,91 @@
-import styled from "styled-components"
-import { GlobalStyle } from "../styledGlobal"
-import fundo from "../assets/image1.png"
+import styled from "styled-components";
+import { GlobalStyle } from "./GlobalStyle";
+import fundo from './assets/loginFundo.png'
 
-const ImgBackground = styled.div`
-    background-image: url(${fundo});
-    width: 100%;
-    min-height: 100vh;
-    background-size: cover;
+
+const Background = styled.div`
+  background-image: url(${fundo});
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 100%;
+  min-height: 100vh;
+`
+
+export const Titulo = styled.h1`
+    color: #FF79C6;
+    text-align: center;
+`
+
+export const FormLogin = styled.div`
+    background-color: #00A3FF;
+    width: 30%;
+    margin-top: 5%;
+    margin-left: 10%;
+    padding: 20px;
+    border-radius: 30px;
+    border-style: solid;
+    border-color: #00A3FF;
+    border-width: 4px;
+    text-align: center;
+`
+
+export const CardTitle = styled.h2`
+    color: black;
+    text-align: center;
+`
+
+export const FormInput = styled.input`
+    width: 80%;
+    height: 40px;
+    border-radius: 20px;
+    margin-bottom: 40px;
+    font-size: 16px;
+    border: none;
+    padding: 15px;
+`
+
+export const FormSubmit = styled.button`
+    display: block;
+    width: 50%;
+    height: 30px;
+    font-size: 18px;
+    font-weight: 900;
+    background-color: #ECE200;
+    border-radius: 20px;
+    margin-bottom: 20px;
+    border-style: none;
+    margin-left: 25%;
+    margin-right: 25%;
+    &:hover {
+      background-color: #FFB800;
+      transform: scale(1.05, 1.05);
+      transition: 1s;
+
+    }
 `
 
 
-
-export default function Login(){
-    return (
+function App() {
+  return (
     <>
     <GlobalStyle/>
-    <ImgBackground>
-        
-    </ImgBackground>
+    <Background>
+    <Titulo>Ojisdji</Titulo>
+    <FormLogin>
+
+      <CardTitle>Username</CardTitle>
+      <FormInput></FormInput>
+      <CardTitle>Email</CardTitle>
+      <FormInput></FormInput>
+      <CardTitle>Senha</CardTitle>
+      <FormInput></FormInput>
+      <FormSubmit type='submit'>Enviar</FormSubmit>
+    </FormLogin>
+    
+    </Background>
+    
     </>
-    )
+  );
 }
+
+export default App;
