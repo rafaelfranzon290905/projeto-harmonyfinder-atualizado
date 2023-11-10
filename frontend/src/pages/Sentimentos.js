20aef0da6c7e935f90329d77a74e1783fc6dee5b
Rafael da Silva Franzon <rafaelfranzon2909@gmail.com>
2023-11-10 09:11:56 -0300

    correção da sora Paula - useState

--- frontend/src/pages/Sentimentos.js
+++ frontend/src/pages/Sentimentos.js
@@ -1,4 +1,4 @@
-import { Background} from '../styledGlobal';
+import { Background } from '../styledGlobal';
 import styled from "styled-components"
 import logo from '../assets/senac_logo_branco.png';
 import profile from '../assets/image-removebg-preview (3).png';
@@ -6,7 +6,7 @@ import sorriso from '../assets/sorriso.png';
 import triste from '../assets/triste.png';
 import paixao from '../assets/paixão.png';
 import raiva from '../assets/raiva.png';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
 import {
@@ -15,7 +15,7 @@ import {
     AccordionButton,
     AccordionPanel,
     AccordionIcon,
-  } from '@chakra-ui/react'
+} from '@chakra-ui/react'
 
 import { Box } from '@chakra-ui/react'
 
@@ -118,13 +118,20 @@ export default function Sentimentos() {
 
     const [emocao, setEmocao] = useState()
 
+    console.log(emocao)
+
+    const navigate = useNavigate()
+
+
+   
+
     return (
         <>
             <Background>
                 <Header>
                     <BlocoHeader>
-                        <HeaderImg src={logo} class='logo' alt=""/>
-                        <HeaderImg src={profile} class='perfil' alt=""/>
+                        <HeaderImg src={logo} class='logo' alt="" />
+                        <HeaderImg src={profile} class='perfil' alt="" />
                     </BlocoHeader>
                 </Header>
 
@@ -133,52 +140,67 @@ export default function Sentimentos() {
                     <Accordion allowToggle>
                         <AccordionItem>
                             <Titulo>
-                            <AccordionButton>
-                                <Box as="h2" flex='1' textAlign='left'>
-                                Selecione o sentimento desejado:
-                                </Box>
-                                <AccordionIcon />
-                            </AccordionButton>
+                                <AccordionButton>
+                                    <Box as="h2" flex='1' textAlign='left'>
+                                        Selecione o sentimento desejado:
+                                    </Box>
+                                    <AccordionIcon />
+                                </AccordionButton>
                             </Titulo>
                             <AccordionPanel pb={4}>
                                 <DivCaixaSentimento>
-                                    <LinkStyle to='/paletas'><CaixaSentimento1 id='feliz' onClick={() => setEmocao("feliz")}><SentimentoEmoji alt='' src={sorriso}/></CaixaSentimento1></LinkStyle>
-                                    <LinkStyle to='/paletas'></LinkStyle><CaixaSentimento2 id='triste' onClick={() => setEmocao("triste")}><SentimentoEmoji alt='' src={triste}/></CaixaSentimento2><LinkStyle/>
-                                    <LinkStyle to='/paletas'></LinkStyle><CaixaSentimento3 id='paixao' onClick={() => setEmocao("paixao")}><SentimentoEmoji alt='' src={paixao}/></CaixaSentimento3><LinkStyle/>
-                                    <LinkStyle to='/paletas'></LinkStyle><CaixaSentimento4 id='raiva' onClick={() => setEmocao("raiva")}><SentimentoEmoji alt='' src={raiva}/></CaixaSentimento4><LinkStyle/>
+
+                                        <CaixaSentimento1 id='feliz' onClick={() => setEmocao("feliz")}>
+                                    <div onClick={()=>{navigate('/paletas')}}>
+                                            <SentimentoEmoji alt='' src={sorriso}/>
+                                    </div>
+                                        </CaixaSentimento1>
+                                     
                                     
+                                    {/* <CaixaSentimento1 id='feliz' onClick={() => setEmocao("feliz")}> */}
+
+                                        {/* <LinkStyle to='/paletas'>
+                                        <SentimentoEmoji alt='' src={sorriso} />
+                                        </LinkStyle> */}
+                                    {/* </CaixaSentimento1> */}
+
+
+                                    <LinkStyle to='/paletas'></LinkStyle><CaixaSentimento2 id='triste' onClick={() => setEmocao("triste")}><SentimentoEmoji alt='' src={triste} /></CaixaSentimento2><LinkStyle />
+                                    <LinkStyle to='/paletas'></LinkStyle><CaixaSentimento3 id='paixao' onClick={() => setEmocao("paixao")}><SentimentoEmoji alt='' src={paixao} /></CaixaSentimento3><LinkStyle />
+                                    <LinkStyle to='/paletas'></LinkStyle><CaixaSentimento4 id='raiva' onClick={() => setEmocao("raiva")}><SentimentoEmoji alt='' src={raiva} /></CaixaSentimento4><LinkStyle />
+
                                 </DivCaixaSentimento>
-                                
+
                             </AccordionPanel>
                         </AccordionItem>
 
                         <AccordionItem>
                             <Titulo>
-                            <AccordionButton>
-                                <Box as="h2" flex='1' textAlign='left'>
-                                Selecione a cor desejada:
-                                </Box>
-                                <AccordionIcon />
-                            </AccordionButton>
+                                <AccordionButton>
+                                    <Box as="h2" flex='1' textAlign='left'>
+                                        Selecione a cor desejada:
+                                    </Box>
+                                    <AccordionIcon />
+                                </AccordionButton>
                             </Titulo>
                             <AccordionPanel pb={4}>
-                            <DivCaixaSentimento>
+                                <DivCaixaSentimento>
 
-                            <LinkStyle to='/paletas'><SelecionarCorAmarelo/></LinkStyle>
-                            <LinkStyle to='/paletas'><SelecionarCorAzul/></LinkStyle>
-                            <LinkStyle to='/paletas'><SelecionarCorRosa/></LinkStyle>
-                            <LinkStyle to='/paletas'><SelecionarCorVermelho/></LinkStyle>
+                                    <LinkStyle to='/paletas'><SelecionarCorAmarelo /></LinkStyle>
+                                    <LinkStyle to='/paletas'><SelecionarCorAzul /></LinkStyle>
+                                    <LinkStyle to='/paletas'><SelecionarCorRosa /></LinkStyle>
+                                    <LinkStyle to='/paletas'><SelecionarCorVermelho /></LinkStyle>
 
-                            </DivCaixaSentimento>
-                        
+                                </DivCaixaSentimento>
+
 
                             </AccordionPanel>
                         </AccordionItem>
-                        </Accordion>
-                    </BlocoEscolha>
+                    </Accordion>
+                </BlocoEscolha>
             </Background>
         </>
-       
+
     )
     console.log(emocao)
 }
